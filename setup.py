b474f5695b1dcffc696ae6d1dfb00e30dd744026
movoid <bobrobotsun@163.com>
2024-05-11 04:22:06 +0800

    2024.05.11.01-movoid 更新1.5.5 1、确认更新

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding='utf-8') as fh:
 
 setup(
     name='movoid_function',
-    version='1.5.4.001',
+    version='1.5.5',
     packages=find_packages(),
     url='',
     license='',
