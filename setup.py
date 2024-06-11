ed13a85c2c68b46c3216e65e814ca2d2781aa4fa
movoid <bobrobotsun@163.com>
2024-06-11 23:24:50 +0800

    2024.06.11.01-movoid 更新1.6.2 1、确认升级，发布包

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding='utf-8') as fh:
 
 setup(
     name='movoid_function',
-    version='1.6.1.002',
+    version='1.6.2',
     packages=find_packages(),
     url='',
     license='',
