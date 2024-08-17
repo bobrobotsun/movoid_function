eccab7f9c7fe1b9ff504616907982e11ef073b6b
bobrobotsun <bobrobotsun@163.com>
2024-08-18 00:43:28 +0800

    2024.08.18.01-movoid 测试辅助函数错误，修复错误

--- test/test_builtin.py
+++ test/test_builtin.py
@@ -2,11 +2,11 @@ import builtins
 import sys
 
 
-def test_error(text):
+def do_error(text):
     print(text, file=sys.stderr)
 
 
 class Test_builtins:
     def test_01_replace_print(self):
-        setattr(builtins, 'error', test_error)
+        setattr(builtins, 'error', do_error)
         error(123)
