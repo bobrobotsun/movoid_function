31578d23cc09b4b87439a15a1f72b9e610b697d1
bobrobotsun <bobrobotsun@163.com>
2025-11-29 01:02:53 +0800

    2025.11.29.2 正式升级1.8.0 增加了STACK专用于针对栈的一些处理，包括跳转栈的信息

--- test/test_stack.py
+++ test/test_stack.py
@@ -1,3 +1,4 @@
+import pathlib
 import sys
 
 from movoid_function import STACK, wraps
@@ -6,19 +7,19 @@ from movoid_function import STACK, wraps
 class Test_class_Stack:
     def test_01_STACK_initial_ignore_list(self):
         assert len(STACK.ignore_list) == 6
-        assert STACK.ignore_list[0] == (r'E:\000develop\movoid_function\movoid_function\decorator.py', 377, None)
-        assert STACK.ignore_list[1] == (r"E:\000develop\movoid_function\movoid_function\decorator.py", 668, None)
-        assert STACK.ignore_list[2] == (r"E:\000develop\movoid_function\movoid_function\decorator.py", 716, None)
-        assert STACK.ignore_list[3] == (r"E:\000develop\movoid_function\movoid_function\decorator.py", 763, None)
-        assert STACK.ignore_list[4] == (r"E:\000develop\movoid_function\movoid_function\decorator.py", 432, None)
-        assert STACK.ignore_list[5] == (r"E:\000develop\movoid_function\movoid_function\decorator.py", 488, None)
+        assert STACK.ignore_list[0] == (str(pathlib.Path(__file__).parent.parent/'movoid_function'/'decorator.py'), 375, None)
+        assert STACK.ignore_list[1] == (str(pathlib.Path(__file__).parent.parent/'movoid_function'/'decorator.py'), 666, None)
+        assert STACK.ignore_list[2] == (str(pathlib.Path(__file__).parent.parent/'movoid_function'/'decorator.py'), 714, None)
+        assert STACK.ignore_list[3] == (str(pathlib.Path(__file__).parent.parent/'movoid_function'/'decorator.py'), 761, None)
+        assert STACK.ignore_list[4] == (str(pathlib.Path(__file__).parent.parent/'movoid_function'/'decorator.py'), 430, None)
+        assert STACK.ignore_list[5] == (str(pathlib.Path(__file__).parent.parent/'movoid_function'/'decorator.py'), 486, None)
 
     def test_02_get_stack_info(self):
-        assert STACK.get_simple_frame_info(0) == (__file__, 17, 'test_02_get_stack_info')
+        assert STACK.get_simple_frame_info(0) == (__file__, 18, 'test_02_get_stack_info')
 
     def test_03___call__(self):
         stack = STACK(sys._getframe())
-        assert stack.info == (__file__, 20, 'test_03___call__')
+        assert stack.info == (__file__, 21, 'test_03___call__')
 
     def test_04_dec_info(self):
         print_list = []
@@ -48,13 +49,13 @@ class Test_class_Stack:
 
         STACK.file_should_ignore(__file__, 're_value = func(*args, **kwargs)')
         temp2()
-        assert print_list[0] == ('E:\\000develop\\movoid_function\\test\\test_stack.py', 43, 'temp2')
-        assert print_list[1] == ('E:\\000develop\\movoid_function\\test\\test_stack.py', 50, 'test_04_dec_info')
+        assert print_list[0] == (__file__, 44, 'temp2')
+        assert print_list[1] == (__file__, 51, 'test_04_dec_info')
         assert print_list[2] == 'temp2 start'
-        assert print_list[3] == ('E:\\000develop\\movoid_function\\test\\test_stack.py', 36, 'temp1')
-        assert print_list[4] == ('E:\\000develop\\movoid_function\\test\\test_stack.py', 46, 'temp2')
+        assert print_list[3] == (__file__, 37, 'temp1')
+        assert print_list[4] == (__file__, 47, 'temp2')
         assert print_list[5] == 'temp1 start'
-        assert print_list[6] == ('E:\\000develop\\movoid_function\\test\\test_stack.py', 39, 'temp1')
-        assert print_list[7] == ('E:\\000develop\\movoid_function\\test\\test_stack.py', 46, 'temp2')
+        assert print_list[6] == (__file__, 40, 'temp1')
+        assert print_list[7] == (__file__, 47, 'temp2')
         assert print_list[8] == 'temp1 end'
         assert print_list[9] == 'temp2 end'
